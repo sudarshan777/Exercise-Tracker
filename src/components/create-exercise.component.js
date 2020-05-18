565d0bd679086b2ae564ca96250652bbb8855d17
Sudarshan Agrawal <sudarshan.shubham@gmail.com>
2020-05-18 17:13:23 -0400

    initial commit

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+export default class CreateExercise extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      description: "",
+      duration: 0,
+      date: new Date(),
+      users: [],
+    };
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value,
+    });
+  }
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value,
+    });
+  }
+  onChangeDate(e) {
+    this.setState({
+      date: e.target.value,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <p>You are on the Create Exercises component!</p>
+      </div>
+    );
+  }
+}
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class ExercisesList extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are on the Exercises List component!</p>
+            </div>
+        )
+    }
+}
+
+
